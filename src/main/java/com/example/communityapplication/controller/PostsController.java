330459fb2d8f6cc861de739a311f7bd448bb905e
Stella <yeobinlee1210@gmail.com>
2026-07-04 16:59:50 +0900

    feat(PostsController): ResponseEntity 도입

--- src/main/java/com/example/communityapplication/controller/PostsController.java
+++ src/main/java/com/example/communityapplication/controller/PostsController.java
@@ -5,6 +5,8 @@ import com.example.communityapplication.response.ApiResponse;
 import com.example.communityapplication.service.PostService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,35 +17,39 @@ public class PostsController {
     private final PostService postService;
 
     @PostMapping("/{userId}")
-    public ApiResponse<PostResponseDto> createPost(@PathVariable Long userId, @Valid @RequestBody PostRequestDto request) {
+    public ResponseEntity<ApiResponse<PostResponseDto>> createPost(@PathVariable Long userId, @Valid @RequestBody PostRequestDto request) {
         PostResponseDto postResponse = postService.createPost(userId, request.getTitle(),request.getContent(),request.getFile());
-        return ApiResponse.of("post_success", postResponse);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(ApiResponse.of("post_success", postResponse));
     }
 
     //전체 조회
     @GetMapping
-    public ApiResponse<PostsListResponseDto> getPostList() {
-     PostsListResponseDto postLististResponse = postService.getPostList();
-        return ApiResponse.of("get_success", postLististResponse);
+    public ResponseEntity<ApiResponse<PostsListResponseDto>> getPostList() {
+        PostsListResponseDto postLististResponse = postService.getPostList();
+        return ResponseEntity
+                .ok(ApiResponse.of("get_success", postLististResponse));
     }
 
     //상세 조회
     @GetMapping("/{postId}")
-    public ApiResponse<PostResponseDto> getPost(@PathVariable Long postId) {
+    public ResponseEntity<ApiResponse<PostResponseDto>> getPost(@PathVariable Long postId) {
         PostResponseDto postResponse = postService.getPost(postId);
-        return ApiResponse.of("get_success", postResponse);
+        return ResponseEntity
+                .ok(ApiResponse.of("get_success", postResponse));
     }
 
     //게시글 수정
     @PatchMapping("/{postId}")
-    public ApiResponse<PostUpdateResponseDto> updatePost(@PathVariable Long postId, @Valid  @RequestBody PostUpdateRequestDto request) {
+    public ResponseEntity<ApiResponse<PostUpdateResponseDto>> updatePost(@PathVariable Long postId, @Valid  @RequestBody PostUpdateRequestDto request) {
         PostUpdateResponseDto postResponse = postService.updatePost(postId, request.getTitle(),request.getContent(),request.getFile());
-        return ApiResponse.of("patch_success", postResponse);
+        return ResponseEntity.ok(ApiResponse.of("patch_success", postResponse));
     }
 
     @DeleteMapping("/{postId}")
-    public ApiResponse<EmptyResponseDto> deletePost(@PathVariable Long postId) {
+    public ResponseEntity<ApiResponse<EmptyResponseDto>> deletePost(@PathVariable Long postId) {
         postService.deletePost(postId);
-        return ApiResponse.of("delete_success", new EmptyResponseDto());
+        return ResponseEntity.ok(ApiResponse.of("delete_success", new EmptyResponseDto()));
     }
 }
