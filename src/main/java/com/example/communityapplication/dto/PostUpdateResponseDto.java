f6f06f8f42ecd21f38fcc1bd280c5e1696ca8a1f
Stella <yeobinlee1210@gmail.com>
2026-07-04 19:19:24 +0900

    refactor(PostDto): Bean Validation 수정

--- src/main/java/com/example/communityapplication/dto/PostUpdateResponseDto.java
+++ src/main/java/com/example/communityapplication/dto/PostUpdateResponseDto.java
@@ -1,6 +1,8 @@
 package com.example.communityapplication.dto;
 
 import com.example.communityapplication.entity.Posts;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -9,12 +11,14 @@ import java.util.Date;
 @Getter
 @NoArgsConstructor
 public class PostUpdateResponseDto {
+    @Size(min=1)
+    @Size(max=26)
     private String title;
     private String content;
     private String file;
+    @NotNull
     private Date updatedAt;
     public PostUpdateResponseDto(Posts posts){
-
         this.updatedAt = posts.getUpdatedAt();
         this.title = posts.getTitle();
         this.content = posts.getContent();
--- src/main/java/com/example/communityapplication/dto/ProfilePictureResponseDto.java
+++ src/main/java/com/example/communityapplication/dto/ProfilePictureResponseDto.java
@@ -1,11 +1,13 @@
 package com.example.communityapplication.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 public class ProfilePictureResponseDto {
+    @NotBlank
     private String profilePicture;
     public ProfilePictureResponseDto(String profilePicture){
         this.profilePicture = profilePicture;
