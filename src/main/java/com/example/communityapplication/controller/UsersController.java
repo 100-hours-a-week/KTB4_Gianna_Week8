ea7ecb74972bdd561f47f7fa459a8e3efb615dc4
Stella <yeobinlee1210@gmail.com>
2026-07-04 16:54:35 +0900

    feat(UsersController): ResponseEntity 도입

--- src/main/java/com/example/communityapplication/controller/UsersController.java
+++ src/main/java/com/example/communityapplication/controller/UsersController.java
@@ -5,6 +5,8 @@ import com.example.communityapplication.response.ApiResponse;
 import com.example.communityapplication.service.UsersService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,60 +17,65 @@ public class UsersController {
     private final UsersService usersService;
 
     @PostMapping("/signup")
-    public ApiResponse<UserResponseDto> createUser(@Valid @RequestBody UserRequestDto request) {
+    public ResponseEntity<ApiResponse<UserResponseDto>> createUser(@Valid @RequestBody UserRequestDto request) {
         UserResponseDto userResponseDto;
         try {
             userResponseDto = usersService.create(request.getEmail(), request.getPassword(),request.getNickname(),request.getProfilePicture());
         } catch (IllegalAccessException e) {
             throw new RuntimeException(e);
         }
-        return ApiResponse.of("signup_success", userResponseDto);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(ApiResponse.of("signup_success", userResponseDto));
     }
 
     @PostMapping("/login")
-    public ApiResponse<LoginResponseDto> userLogin(@Valid @RequestBody LoginRequestDto request) throws IllegalAccessException {
+    public ResponseEntity<ApiResponse<LoginResponseDto>> userLogin(@Valid @RequestBody LoginRequestDto request) throws IllegalAccessException {
         LoginResponseDto userResponse;
         try {
             userResponse = usersService.userLogin(request.getEmail(), request.getPassword());
         } catch (IllegalAccessException e) {
             throw new IllegalAccessException(e.getMessage());
         }
-        return ApiResponse.of("login_sucess", userResponse);
+        return ResponseEntity
+                .ok(ApiResponse.of("login_sucess", userResponse));
     }
 
     @GetMapping("/{userId}")
-    public ApiResponse<UserResponseDto> getUser(@PathVariable Long userId){
+    public ResponseEntity<ApiResponse<UserResponseDto>> getUser(@PathVariable Long userId){
         UserResponseDto userResponse =  usersService.getUser(userId);
-        return ApiResponse.of("get_data_success", userResponse);
+        return ResponseEntity
+                .ok(ApiResponse.of("get_data_success", userResponse));
     }
 
     @GetMapping("/{userId}/profilePicture")
-    public ApiResponse<ProfilePictureResponseDto> getProfilePicture (@PathVariable Long userId){
+    public ResponseEntity<ApiResponse<ProfilePictureResponseDto>> getProfilePicture (@PathVariable Long userId){
         ProfilePictureResponseDto profilePictureResponse = usersService.getUserProfilePicture(userId);
-        return ApiResponse.of("get_profile_picture_success", profilePictureResponse);
+        return ResponseEntity
+                .ok(ApiResponse.of("get_profile_picture_success", profilePictureResponse));
     }
 
     @PatchMapping("/{userId}/nickname")
-    public ApiResponse<EmptyResponseDto> updateNickname(@PathVariable Long userId, @Valid  @RequestBody UserUpdateRequestDto request){
+    public ResponseEntity<ApiResponse<EmptyResponseDto>> updateNickname(@PathVariable Long userId, @Valid  @RequestBody UserUpdateRequestDto request){
         usersService.updateNickname(userId, request.getNickname());
-        return ApiResponse.of("patch_success", new EmptyResponseDto());
+        return ResponseEntity.noContent().build();
     }
 
     @PatchMapping("/{userId}/password")
-    public ApiResponse<EmptyResponseDto> updatePassword(@PathVariable Long userId, @Valid  @RequestBody UserUpdateRequestDto request){
+    public ResponseEntity<ApiResponse<EmptyResponseDto>> updatePassword(@PathVariable Long userId, @Valid  @RequestBody UserUpdateRequestDto request){
         usersService.updatePassword(userId, request.getPassword());
-        return ApiResponse.of("patch_success", new EmptyResponseDto());
+        return ResponseEntity.noContent().build();
     }
 
     @PatchMapping("/{userId}/profilePicture")
-    public ApiResponse<EmptyResponseDto> updateProfilePicture(@PathVariable Long userId, @Valid  @RequestBody UserUpdateRequestDto request){
+    public ResponseEntity<ApiResponse<EmptyResponseDto>> updateProfilePicture(@PathVariable Long userId, @Valid  @RequestBody UserUpdateRequestDto request){
         usersService.updateProfilePicture(userId, request.getProfilePicture());
-        return ApiResponse.of("patch_success", new EmptyResponseDto());
+        return ResponseEntity.noContent().build();
     }
 
     @DeleteMapping("/{userId}")
-    public ApiResponse<EmptyResponseDto> deleteUser(@PathVariable Long userId){
+    public ResponseEntity<ApiResponse<EmptyResponseDto>> deleteUser(@PathVariable Long userId){
         usersService.deleteUser(userId);
-        return ApiResponse.of("delete_success", new EmptyResponseDto());
+        return ResponseEntity.noContent().build();
     }
 }
