c34ef643e92143584ab564a6d8a937264e9fcec1
Stella <yeobinlee1210@gmail.com>
2026-07-13 22:30:04 +0900

    fix(): updatePasswordTest_Success에 있던 password encoding 오류 수정 및 테스트 이름 오류 수정

--- src/main/java/com/example/communityapplication/config/SecurityConfig.java
+++ src/main/java/com/example/communityapplication/config/SecurityConfig.java
@@ -29,7 +29,6 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http){
         //접근 허용 제한 - URL 기반 보안
         http
-                .csrf(csrf->csrf.disable())
                 .cors(cors -> cors.configurationSource(corsConfigurationSource())) // CORS 설정 추가
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers( "/users/login", "/users/signup", "/login-process").permitAll() // 모두에게 허용
--- src/test/java/com/example/communityapplication/service/UserServiceTest.java
+++ src/test/java/com/example/communityapplication/service/UserServiceTest.java
@@ -29,8 +29,9 @@ public class UserServiceTest {
     @DisplayName("User Service Test - updateNickname 비즈니스 로직 단위 테스트")
     void updateNicknameTest_Success(){
         //given
-        Users user = new Users("test@test.com", passwordEncoder.encode("Test!234"), "testuser","image.jpg");
+        Users user = new Users("test@test.com", "eNcOdEdPaSsWoRd", "testuser","image.jpg");
         String newNickname = "newName";
+        //updateNickname 함수 내부에 보면 repository 조회를 하는 로직이 있다. 그 때의 반환값을 설정해주는 역할
         when(usersRepository.findById(1L)).thenReturn(Optional.of(user));
 
         //when
@@ -44,23 +45,24 @@ public class UserServiceTest {
     @DisplayName("User Service Test - updatePassword 비즈니스 로직 단위 테스트")
     void updatePasswordTest_Success(){
         //given
-        Users user = new Users("test@test.com", passwordEncoder.encode("Test!234"), "testuser","image.jpg");
-        String expectedPassword = "TEst!234";
-        String encodedPassword = passwordEncoder.encode(expectedPassword);
+        Users user = new Users("test@test.com", "oldPassword", "testuser","image.jpg");
+
+        String newPassword = "newPassword";
+        String encodedPassword = "eNcOdEdPaSsWoRd";
+        when(passwordEncoder.encode(newPassword)).thenReturn(encodedPassword);
         when(usersRepository.findById(1L)).thenReturn(Optional.of(user));
 
         //when
-        usersService.updatePassword( 1L , encodedPassword);
-
+        usersService.updatePassword( 1L , newPassword);
         //then
         assertEquals(encodedPassword, user.getPassword());
     }
 
     @Test
     @DisplayName("User Service Test - updateProfilePicture 비즈니스 로직 단위 테스트")
-    void updatePasswordPicture_Success(){
+    void updateProfilePicture_Success(){
         //given
-        Users user = new Users("test@test.com", passwordEncoder.encode("Test!234"), "testuser","image.jpg");
+        Users user = new Users("test@test.com", "eNcOdEdPaSsWoRd", "testuser","image.jpg");
         String newProfilePicture = "new_image.jpg";
         when(usersRepository.findById(1L)).thenReturn(Optional.of(user));
 
